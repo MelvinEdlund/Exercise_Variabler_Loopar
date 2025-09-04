371879870f132f59eb573f2f69acf93a6c9e40c1
Melvinedl <medlund01@gmail.com>
2025-09-04 20:38:08 +0200

    Add project files.

---
+++ exercise_Variabler/Program.cs
@@ -0,0 +1,336 @@
+﻿//  1.
+//Console.Write("Skriv ditt namn: ");
+//string namn = Console.ReadLine().ToLower();
+//Console.WriteLine($"Hej {namn}!");
+
+
+//2.
+//Console.Write("Skriv in första talet: ");
+//int num1 = Int32.Parse(Console.ReadLine());
+
+//Console.Write("Skriv in andra talet: ");
+//int num2 = Int32.Parse(Console.ReadLine());
+//int sum = num1 * num2;
+//Console.WriteLine($"{num1} * {num2} = {sum}");
+
+//Console.Write("Skriv in första talet: ");
+//if (int.TryParse(Console.ReadLine(), out int num1))
+//{
+//    Console.Write("Skriv in andra talet: ");
+//    if (int.TryParse(Console.ReadLine(), out int num2))
+//    {
+//        Console.WriteLine($"{num1} * {num1 * num2}");
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("Fel: Första talet var inte ett heltal.");
+//}
+
+
+//  3.
+//string password = "Svardfisk";
+//Console.Write("Lösenord: ");
+//string tryPass = Console.ReadLine();
+//Console.WriteLine(tryPass == password ? "Rätt lösenord!" : "Fel lösenord!");
+
+//  4.
+//Console.Write("Skriv ett tal: ");
+//if (int.TryParse(Console.ReadLine(), out int num1))
+//{
+//    if (num1 == 100)
+//    {
+//        Console.WriteLine("Ditt tal är 100");
+//    }
+//    else if (num1 < 100)
+//    {
+//        Console.WriteLine("Ditt tal är mindre än 100");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Ditt tal är större än 100");
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("Fel: talet är inte ett heltal");
+//}
+
+
+//  5.
+//Console.Write("Skriv in ett tal: ");
+//if (double.TryParse(Console.ReadLine(), out double tal))
+//{
+//    Console.WriteLine($"{tal} är dubbelt så mycket som {tal / 2}");
+//    Console.WriteLine($"{tal} är hälften så mycket som {tal * 2}");
+//}
+//else Console.WriteLine("Fel: Du skrev inte in ett tal..");
+
+
+//  6.
+//Console.Write("Första talet: ");
+//double talEtt = double.Parse(Console.ReadLine());
+
+//Console.Write("Välj +, -, *, /: ");
+//string tecken = Console.ReadLine();
+
+//Console.Write("Andra talet: ");
+//double talTvå = double.Parse(Console.ReadLine());
+
+//switch (tecken)
+//{
+//    case "+":
+//        Console.WriteLine($"{talEtt} + {talTvå} = {talEtt + talTvå}");
+//        break;
+//    case "-":
+//        Console.WriteLine($"{talEtt} - {talTvå} = {talEtt - talTvå}");
+//        break;
+//    case "*":
+//        Console.WriteLine($"{talEtt} * {talTvå} = {talEtt * talTvå}");
+//        break;
+//    case "/":
+//        Console.WriteLine($"{talEtt} / {talTvå} = {talEtt / talTvå}");
+//        break;
+//    default:
+//        Console.WriteLine("Fel tecken.");
+//        break;
+//}
+
+
+//  7.
+//double summa = 0;
+//int antal = 0;
+//Console.Write("Skriv in ett tal: ");
+
+//while (double.TryParse(Console.ReadLine(), out double tal))
+//{
+//    summa += tal;
+//    antal++;
+//    Console.WriteLine(summa);
+//    Console.Write("Skriv in ett tal: ");
+//}
+
+//double medel = summa / antal;
+//Console.WriteLine($"Medelvärde: {medel}");
+
+
+
+////// loops ///////
+
+
+//  1.
+//int i = 20;
+//while (i <= 30)
+//{
+//    Console.WriteLine(i);
+//    i++;
+//}
+
+
+//  2.
+//int i = 0;
+//while (i<= 30)
+//{
+//    Console.WriteLine(i);
+//    i++;
+//    i++;
+//}
+
+
+//  3.
+//int i = 0;
+//while (i <= 30)
+//{
+// if(i % 3 == 0 && i != 0)
+//    {
+//        Console.WriteLine("Hej");
+//    }
+// else
+//    {
+//        Console.WriteLine(i);
+//    }
+//    i++;
+//}
+
+
+//  4.
+
+
+//int i = 1;
+//while (true)
+//{ 
+//    Console.Write("Skriv in ett tal: ");
+//    if (double.TryParse(Console.ReadLine(), out double num1))
+//    {
+//        while (i <= 10)
+//        {
+//            Console.WriteLine($"{i} * {num1} = {i * num1}");
+//            i++;
+//        }
+//        break;
+//    }
+//    else Console.WriteLine("Du skrev inte in ett tal..");
+//}
+
+//for (int i = 1; i <= 10; i++)
+//{
+//    Console.WriteLine($"{i} * {num1} = {i * num1}");
+//}
+
+
+//  5.
+//int i = 0;
+//int sum = 0;
+//while (i <= 1000)
+//{
+//    sum += i;
+//    i++;
+
+
+//}
+//Console.WriteLine(sum);
+
+
+//  6.
+//int i = 1;
+//BigInteger sum = 1;
+
+//while (i <= 64)
+//{
+
+
+//    Console.WriteLine($"Ruta {i}: {sum}");
+//    sum *= 2;
+//    i++;
+//}
+
+
+//  7.
+//Console.Write("Mata in höjd: ");
+//int höjd = Int32.Parse(Console.ReadLine());
+//Console.Write("Mata in bredd: ");
+//int bredd = Int32.Parse(Console.ReadLine());
+//int i = 0;
+//int j = 0;
+//string xBredd = string.Empty;
+
+//while (j < bredd)
+//{
+//    xBredd += "X";
+//    j++;
+//}
+
+//while (i < höjd)
+//{
+
+//    Console.WriteLine(xBredd);
+//    i++;
+//}
+
+
+//  8.
+//Console.Write("Mata in höjd: ");
+//int höjd = Int32.Parse(Console.ReadLine());
+//Console.Write("Mata in bredd: ");
+//int bredd = Int32.Parse(Console.ReadLine());
+//int i = 0;
+//int j = 0;
+//string xBredd = string.Empty;
+
+//while (j < bredd)
+//{
+//    if (j % 2 == 0)
+//    {
+//        xBredd += "X";
+//    }
+//    else
+//    {
+//        xBredd += "O";
+//    }
+//    j++;
+
+//}
+
+//while (i < höjd)
+//{
+
+//    Console.WriteLine(xBredd);
+//    i++;
+//}
+
+
+//  9.
+//Console.Write("Mata in höjd: ");
+//int höjd = Int32.Parse(Console.ReadLine());
+//Console.Write("Mata in bredd: ");
+//int bredd = Int32.Parse(Console.ReadLine());
+//int i = 0;
+//int j = 0;
+//int y = 0;
+//string xBredd = string.Empty;
+//string oBredd = string.Empty;
+
+//while (j < bredd)
+//{
+//    if (j % 2 == 0)
+//    {
+//        xBredd += "X";
+//    }
+//    else
+//    {
+//        xBredd += "O";
+//    }
+//    j++;
+
+//}
+//while (y < bredd)
+//{
+//    if (y % 2 == 0)
+//    {
+//        oBredd += "O";
+//    }
+//    else
+//    {
+//        oBredd += "X";
+//    }
+//    y++;
+
+//}
+
+//while (i < höjd)
+//{
+//    if (i % 2 == 0)
+//    {
+//        Console.WriteLine(xBredd);
+//    }
+//    else
+//    {
+//        Console.WriteLine(oBredd);
+//    }
+
+//    i++;
+//}
+
+
+//  10.
+
+
+
+//  11.
+//int i = 1;
+//string adderadeTal = string.Empty;
+//while (i <= 9)
+//{
+
+//    string num = i.ToString();
+//    adderadeTal += num;
+//    Console.WriteLine(adderadeTal);
+//    i++;
+//}
+//string addNumber = string.Empty;
+
+//for (int i = 1; i <= 9; i++)
+//{
+//    addNumber += i.ToString();
+//    Console.WriteLine(addNumber);
+//}
